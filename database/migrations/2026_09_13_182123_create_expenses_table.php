a7feb3943f22e56c669f301ea8246314666255a1
Bapan De <debapan2002@gmail.com>
2026-09-14 00:47:46 +0530

    complete migration

---
+++ database/migrations/2026_09_13_182123_create_expenses_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('category_id')->constrained()->cascadeOnDelete();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->decimal('amount', 10,2);
+            $table->date('date');
+            $table->enum('type',['one_time','recuring'])->default('one_time');
+
+
+            $table->enum('recuring_frequense',['daily','weekly','monthly','yearly'])->nullable();
+            $table->date('recuring_start_date')->nullable();
+            $table->date('recuring_end_date')->nullable();
+            $table->foreignId('parent_expense_id')->nullable()->constrained('expenses')->nullOnDelete();
+            $table->boolean('is_auto_generate')->default(false);
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->index(['user_id','date']);
+            $table->index(['user_id','type']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('expenses');
+    }
+};
